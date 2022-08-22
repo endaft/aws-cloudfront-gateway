d9005c7b8f58bd69757f6b51a655c636cbabca97
Gio Palacino <gio@palacino.net>
2022-08-22 05:24:32 -0700

    feat(origins): removes custom routing for origin group failover

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,13 @@ export async function handler(event: CloudFrontRequestEvent): Promise<CloudFront
   };
 
   try {
-    log('Handling Origin Request Event', event);
+    log('Received Origin Request Event', event);
     const { config, request } = event.Records[0].cf;
     if (config.eventType.toLowerCase() !== 'origin-request') {
       return log('Invalid Event Type', { status: '500', statusDescription: 'Invalid Event Type' });
     }
-    const { s3 } = request.origin;
-    if (!s3) {
+    const { s3, custom } = request.origin;
+    if (!s3 && !custom) {
       return log('Invalid Origin Type', { status: '500', statusDescription: 'Invalid Origin Type' });
     }
 
@@ -30,36 +30,14 @@ export async function handler(event: CloudFrontRequestEvent): Promise<CloudFront
     const subDomain = hostDiff > 0 ? targetHost.substring(0, hostDiff - 1) : 'www';
     const s3Path = path.join(s3.path, subDomain).replace(/\/$/i, '');
 
-    const customHost =
-      s3.customHeaders[`x-origin-${subDomain.toLowerCase()}`]?.[0]?.value ??
-      s3.customHeaders[`X-Origin-${subDomain.toUpperCase()}`]?.[0]?.value ??
-      null;
-    if (!!customHost) {
-      const opts = urlToHttpOptions(new URL(customHost));
-      log('Redirecting Request to Custom Origin');
-      Reflect.deleteProperty(request.origin, 's3');
-      const uri = opts.path.split('/').pop();
-      request.origin.custom = {
-        path: opts.path.split('/').reverse().splice(1).reverse().join('/'),
-        readTimeout: 30,
-        keepaliveTimeout: 5,
-        domainName: opts.hostname,
-        customHeaders: s3.customHeaders,
-        port: parseInt(`${opts.port ?? '443'}`),
-        sslProtocols: ['TLSv1', 'TLSv1.1', 'TLSv1.2'],
-        protocol: 'https',
-      };
-      request.uri = `/${uri}`;
-      request.headers['host'][0].value = opts.hostname;
-    } else {
-      log('Redirecting Request to S3 Origin Path');
-
+    if (!!s3) {
+      log('Updating the S3 Origin Path');
       request.origin.s3.path = s3Path;
       request.headers['host'][0].value = request.origin.s3.domainName;
     }
 
     request.headers['x-target-domain'] = [{ value: targetHost }];
-    return log('Responding with Modified Request', request);
+    return log('Responding with Request', request);
   } catch (e) {
     const errorData = log('Fatal Error Encountered', {
       error: [...((e.stack as string) ?? e.toString()).split('\n')],
